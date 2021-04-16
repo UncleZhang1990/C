330c1a0239a9151e9d4544b3021797932cb32ef1
Uncle_Zhang <Uncle_Zhang1990@163.com>
2021-04-16 17:16:08 +0800

    2021.04.16 for语句

--- for语句/for语句/for语句.cpp
+++ for语句/for语句/for语句.cpp
@@ -1,10 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
-int main(void)
+int main01(void)
 {
-	for (int i = 0; i < 10; i++)
+	int j = 5;
+	for (int i = 0; i < 10, j < 10; i++, j += 5)
 	{
 		printf("%d\n", i);
 	}
+	return 0;
+}
+int main(void)
+{
+	int i = 0;
+	for (; i < 10; i++)
+	{
+		if (i >= 10)
+		{
+			break;
+		}
+		printf("%d\n", i);
+	}
+	return 0;
 }
