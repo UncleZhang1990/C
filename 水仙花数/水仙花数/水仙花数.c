e176112b4238ffb45648ae10cd2218bf47483182
Uncle_Zhang <Uncle_Zhang1990@163.com>
2021-04-16 16:32:08 +0800

    2021.04.16 水仙花数 完成

--- 水仙花数/水仙花数/水仙花数.c
+++ 水仙花数/水仙花数/水仙花数.c
@@ -1,10 +1,22 @@
 //100~999的水仙花数
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
 int main(void)
 {
 	int i = 100;
 	do 
 	{
-	} while ();
+		//个位
+		int a = i % 10;
+		//十位
+		int b = i / 10 % 10;
+		//百位
+		int c = i / 100;
+		if (pow(a, 3) + pow(b, 3) + pow(c, 3) == i)
+		{
+			printf("%d\n", i);
+		}
+		i++;
+	} while (i < 1000);
 }
