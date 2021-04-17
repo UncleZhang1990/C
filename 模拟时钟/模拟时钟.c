04d9fc9a57363184982617ef29edee4ddc4c5697
Uncle_Zhang <Uncle_Zhang1990@163.com>
2021-04-17 10:57:22 +0800

    2021.04.17 模拟时钟

---
+++ 模拟时钟/模拟时钟.c
@@ -0,0 +1,19 @@
+#define _CRT_NONSTDC_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+int main(void)
+{
+	for (int h = 0; h < 24; h++)  //时
+	{
+		for (int m = 0; m < 60; m++)  //分
+		{
+			for (int s = 0; s < 60; s++)  //秒
+			{
+				system("cls");
+				printf("%02d:%02d:%02d\n", h, m, s);  //02 两位数，若不够，用0填充
+				Sleep(950);  //单位 毫秒
+			}
+		}
+	}
+	return 0;
+}
