1ad2db9873dd2d5c790eb5ff57288a2868d6ac2b
UncleZhang <Uncle_Zhang1990@163.com>
2021-04-24 14:34:32 +0800

    2021.04.24 二维数组 学了一半 16：31

---
+++ 二维数组/二维数组.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int arr[2][3] =
+	{
+		{1,2,3},
+		{4,5,6}
+	};
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d\n", arr[i][j]);
+		}
+	}
+	printf("二维数组大小：%d\n", sizeof(arr));
+	printf("二维数组单行大小：%d\n", sizeof(arr[0]));
+	printf("二维数组元素大小：%d\n", sizeof(arr[0][0]));
+	printf("二维数组行数：%d\n", sizeof(arr) / sizeof(arr[0]));
+	printf("二维数组列数：%d\n", sizeof(arr[0]) / sizeof(arr[0][0]));
+	return 0;
+}
