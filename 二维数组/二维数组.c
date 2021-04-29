7f37b590f0d84bc48f3bbca38f9d56fb577c9268
UncleZhang <Uncle_Zhang1990@163.com>
2021-04-29 13:57:35 +0800

    2021.04.29 二维数组

--- 二维数组/二维数组.c
+++ 二维数组/二维数组.c
@@ -19,5 +19,17 @@ int main(void)
 	printf("二维数组元素大小：%d\n", sizeof(arr[0][0]));
 	printf("二维数组行数：%d\n", sizeof(arr) / sizeof(arr[0]));
 	printf("二维数组列数：%d\n", sizeof(arr[0]) / sizeof(arr[0][0]));
+	//二维数组首地址
+	printf("%p\n", &arr);
+	printf("%p\n", &arr[0]);
+	printf("%p\n", &arr[1]);
+	printf("%p\n", &arr[0][0]);
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%p\n", &arr[i][j]);
+		}
+	}
 	return 0;
 }
