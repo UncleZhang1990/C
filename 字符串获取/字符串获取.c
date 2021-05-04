57f622137e91e7bc4841189f0bc91eb3d23aa9e3
UncleZhang <Uncle_Zhang1990@163.com>
2021-05-04 13:24:09 +0800

    2021.05.04 字符串获取

--- 字符串获取/字符串获取.c
+++ 字符串获取/字符串获取.c
@@ -1,7 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+int main0(void)
+{
+	char ch[100];
+	//gets(ch);  //gets接收字符串可带空格
+	//通过正则表达式  接收带空格字符串
+	scanf("%[^\n]", ch);  //[^\n]接收非换行字符
+	printf("%s\n", ch);
+	return 0;
+}
 int main(void)
 {
-
+	char ch1[10];
+	//fgets获取字符串少于元素个数时有\n 大于或等于则没有
+	fgets(ch1, sizeof(ch1), stdin);
+	printf("%s\n", ch1);
 	return 0;
 }
