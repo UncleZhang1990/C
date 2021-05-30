44b518523db15e0ec20a8b6ef11e50ab02b2e8d7
UncleZhang <Uncle_Zhang1990@163.com>
2021-05-30 12:19:44 +0800

    2021.05.31 主函数main和exit函数

---
+++ 主函数main和exit函数/主函数main和exit函数.c
@@ -0,0 +1,14 @@
+//主函数main和exit函数
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	printf("helloworld\n");
+	printf("helloworld\n");
+	//return -1;
+	exit(404);
+	printf("helloworld\n");
+	printf("helloworld\n");
+	printf("helloworld\n");
+	return 0;
+}
